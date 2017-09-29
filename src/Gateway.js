474f03efe23d284e16040eb4b7e02e047f5c4380
David Erenger <david.erenger@briteback.com>
2017-09-29 20:47:11 +0200

    Update Gateway.js

--- src/Gateway.js
+++ src/Gateway.js
@@ -2,19 +2,6 @@ import React from 'react';
 import GatewayRegistry from './GatewayRegistry';
 
 export default class Gateway extends React.Component {
-  static contextTypes = {
-    gatewayRegistry: React.PropTypes.instanceOf(GatewayRegistry).isRequired
-  };
-
-  static propTypes = {
-    into: React.PropTypes.string.isRequired,
-    id: React.PropTypes.string.isRequired,
-    children: React.PropTypes.oneOfType([
-      React.PropTypes.element,
-      React.PropTypes.string
-    ])
-  };
-
   constructor(props, context) {
     super(props, context);
     this.gatewayRegistry = context.gatewayRegistry;
