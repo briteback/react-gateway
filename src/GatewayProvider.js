c76092c2897dc1e2a4c8d2778fbd00295b04b3b0
David Erenger <david.erenger@briteback.com>
2017-09-29 21:59:21 +0200

    Update GatewayProvider.js

--- src/GatewayProvider.js
+++ src/GatewayProvider.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import GatewayRegistry from './GatewayRegistry';
+import { instanceOf } from 'prop-types';
 
 export default class GatewayProvider extends React.Component {
+  static childContextTypes = {		
+    gatewayRegistry: instanceOf(GatewayRegistry).isRequired		
+  };
+
   getChildContext() {
+console.error('getChildContext',  this.gatewayRegistry);
     return {
       gatewayRegistry: this.gatewayRegistry
     };
@@ -11,6 +17,7 @@ export default class GatewayProvider extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.gatewayRegistry = new GatewayRegistry();
+console.error('constructor',  this.gatewayRegistry);
   }
 
   render() {
