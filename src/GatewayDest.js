91b12161c6a3da8510e202027a4a8aee5974aba4
David Erenger <david.erenger@briteback.com>
2017-09-29 20:47:36 +0200

    Update GatewayDest.js

--- src/GatewayDest.js
+++ src/GatewayDest.js
@@ -2,18 +2,6 @@ import React from 'react';
 import GatewayRegistry from './GatewayRegistry';
 
 export default class GatewayDest extends React.Component {
-  static contextTypes = {
-    gatewayRegistry: React.PropTypes.instanceOf(GatewayRegistry).isRequired
-  };
-
-  static propTypes = {
-    name: React.PropTypes.string.isRequired,
-    component: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.func
-    ])
-  };
-
   constructor(props, context) {
     super(props, context);
     this.gatewayRegistry = context.gatewayRegistry;
